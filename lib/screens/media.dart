93d19fdfb0ee75caa3c0e6097e33eef44746cde4
TheProgxy <theprogxy@gmail.com>
2021-01-10 20:14:50 +0100

    add marquee to media

--- lib/screens/media.dart
+++ lib/screens/media.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:marquee_widget/marquee_widget.dart';
 
 import 'home/mainDrawer.dart';
 
@@ -11,6 +13,47 @@ class Media extends StatelessWidget {
         title: Text("Foto e Video"),
       ),
       drawer: MainDrawer(),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            CarouselSlider(
+              items: [
+                Marquee(
+                  child: Text(
+                      'This project is a starting point for a Dart package'),
+                  direction: Axis.horizontal,
+                  textDirection: TextDirection.rtl,
+                  animationDuration: Duration(seconds: 1),
+                  backDuration: Duration(milliseconds: 5000),
+                  pauseDuration: Duration(milliseconds: 2500),
+                  directionMarguee: DirectionMarguee.oneDirection,
+                ),
+              ],
+              options: null,
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            CarouselSlider(
+              items: [
+                Marquee(
+                  child: Text(
+                      'This project is a starting point for a Dart package'),
+                  direction: Axis.horizontal,
+                  textDirection: TextDirection.rtl,
+                  animationDuration: Duration(seconds: 1),
+                  backDuration: Duration(milliseconds: 5000),
+                  pauseDuration: Duration(milliseconds: 2500),
+                  directionMarguee: DirectionMarguee.oneDirection,
+                ),
+              ],
+              options: null,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
