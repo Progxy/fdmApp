a1779c53d02571123ac4fb2e4f97e0000fb1a302
TheProgxy <theprogxy@gmail.com>
2021-01-12 21:57:33 +0100

    fix login.dart

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,16 +1,24 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
 
-import 'home/mainDrawer.dart';
+//import 'home/mainDrawer.dart';
 
-class Login extends StatelessWidget {
-  static const String routeName = "/login";
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-      ),
-      drawer: MainDrawer(),
-    );
-  }
-}
+// class Login extends StatefulWidget {
+//   static const String routeName = "/login";
+
+//   @override
+//   _LoginState createState() => _LoginState();
+// }
+
+// class _LoginState extends State<Login> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Login"),
+//       ),
+//       drawer: MainDrawer(),
+//     );
+//   }
+// }
