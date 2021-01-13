a40399c4a5ff585078d651a7e214a01bebcf5945
TheProgxy <theprogxy@gmail.com>
2021-01-13 19:20:38 +0100

    fix detailed

--- lib/screens/media/detailedPhoto.dart
+++ lib/screens/media/detailedPhoto.dart
@@ -6,9 +6,42 @@ class DetailedPhoto extends StatelessWidget {
   Widget build(BuildContext context) {
     final List details = ModalRoute.of(context).settings.arguments as List;
     final titlePhoto = details[0];
+    final textPhoto = details[3];
     return Scaffold(
       appBar: AppBar(
         title: Text(titlePhoto),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            titlePhoto,
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            textPhoto,
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Divider(
+            height: 2,
+          ),
+          Image.network(
+            details[2],
+          ),
+          SizedBox(
+            height: 25,
+          ),
+        ],
       ),
     );
   }
--- lib/screens/media/detailedVideo.dart
+++ lib/screens/media/detailedVideo.dart
@@ -1,14 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class DetailedVideo extends StatelessWidget {
+class DetailedVideo extends StatefulWidget {
   static const String routeName = "/detailedvideo";
+
+  @override
+  _DetailedVideoState createState() => _DetailedVideoState();
+}
+
+class _DetailedVideoState extends State<DetailedVideo> {
+  YoutubePlayerController _controller;
+  TextEditingController _idController;
+  TextEditingController _seekToController;
+  PlayerState _playerState;
+  YoutubeMetaData _videoMetaData;
+  bool _isPlayerReady = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: "fzoF_KhJO8c",
+      flags: const YoutubePlayerFlags(
+        mute: false,
+        autoPlay: true,
+        disableDragSeek: false,
+        loop: false,
+        isLive: false,
+        forceHD: false,
+        enableCaption: true,
+      ),
+    )..addListener(listener);
+    _idController = TextEditingController();
+    _seekToController = TextEditingController();
+    _videoMetaData = const YoutubeMetaData();
+    _playerState = PlayerState.unknown;
+  }
+
+  void listener() {
+    if (_isPlayerReady && mounted && !_controller.value.isFullScreen) {
+      setState(() {
+        _playerState = _controller.value.playerState;
+        _videoMetaData = _controller.metadata;
+      });
+    }
+  }
+
+  @override
+  void deactivate() {
+    _controller.pause();
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _idController.dispose();
+    _seekToController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final List details = ModalRoute.of(context).settings.arguments as List;
     final videoTitle = details[0];
+    final videoText = details[3];
+    _controller.load(details[2]);
     return Scaffold(
       appBar: AppBar(
         title: Text(videoTitle),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            videoTitle,
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            videoText,
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Divider(
+            height: 3,
+          ),
+          YoutubePlayerBuilder(
+            onExitFullScreen: () {
+              SystemChrome.setPreferredOrientations(DeviceOrientation.values);
+            },
+            player: YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
+              progressIndicatorColor: Colors.blueAccent,
+              topActions: <Widget>[
+                const SizedBox(width: 8.0),
+                Expanded(
+                  child: Text(
+                    _controller.metadata.title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                ),
+              ],
+              onReady: () {
+                _isPlayerReady = true;
+              },
+            ),
+            builder: (context, player) => Container(
+              child: player,
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+        ],
       ),
     );
   }
