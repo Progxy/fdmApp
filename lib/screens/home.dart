aca7a85921f695a4ddab9f31d38dd34ece4a8e74
TheProgxy <theprogxy@gmail.com>
2021-01-15 22:00:55 +0100

    fixed home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,116 +14,128 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  verifyConnection() async {
-    var connectivityResult = await (Connectivity().checkConnectivity());
+  bool isConnected = false;
+
+  void verifyConnection() async {
+    var connectivityResult = await Connectivity().checkConnectivity();
     if (connectivityResult == ConnectivityResult.none) {
-      return false;
+      setState(() {
+        isConnected = false;
+      });
     } else {
-      return true;
+      setState(() {
+        isConnected = true;
+      });
     }
+    print(isConnected);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    verifyConnection();
   }
 
   @override
   Widget build(BuildContext context) {
     final bool isIOS = Theme.of(context).platform == TargetPlatform.iOS;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Fondazione Don Milani App"),
-      ),
-      drawer: MainDrawer(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                verifyConnection()
-                    ? SizedBox(
-                        height: 15,
-                      )
-                    : isIOS
-                        ? showCupertinoDialog(
-                            context: context,
-                            builder: (BuildContext context) =>
-                                CupertinoAlertDialog(
-                              title: Icon(
-                                Icons.error,
-                                size: 40.0,
-                                color: Colors.red,
-                              ),
-                              content: Text(
-                                "Connessione Assente, alcuni servizi potrebbero essere bloccati!",
-                                style: TextStyle(
-                                  fontSize: 27,
-                                ),
-                              ),
-                              actions: [
-                                CupertinoDialogAction(
-                                  child: Text(
-                                    "OK",
-                                    style: TextStyle(
-                                      fontSize: 28,
-                                    ),
-                                  ),
-                                  onPressed: () {
-                                    Navigator.of(context, rootNavigator: true)
-                                        .pop('dialog');
-                                  },
-                                )
-                              ],
-                            ),
-                          )
-                        : showDialog(
-                            barrierDismissible: false,
-                            context: context,
-                            builder: (BuildContext context) => AlertDialog(
-                              title: Icon(
-                                Icons.error,
-                                size: 40.0,
-                                color: Colors.red,
-                              ),
-                              content: Text(
-                                "Connessione Assente, alcuni servizi potrebbero essere bloccati!",
-                                style: TextStyle(
-                                  fontSize: 27,
-                                ),
-                              ),
-                              actions: [
-                                FlatButton(
-                                  child: Text(
-                                    "OK",
-                                    style: TextStyle(
-                                      fontSize: 28,
-                                    ),
-                                  ),
-                                  onPressed: () {
-                                    Navigator.of(context, rootNavigator: true)
-                                        .pop('dialog');
-                                  },
-                                )
-                              ],
-                            ),
-                          ),
-                Image(
-                  image: AssetImage("assets/images/don_milani.png"),
-                  fit: BoxFit.cover,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 30, left: 15, right: 20),
-                  child: MainNews(),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 30, left: 15, right: 20),
-                  child: MainBarbiana(),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 25,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Fondazione Don Milani App"),
         ),
-      ),
-    );
+        drawer: MainDrawer(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  isConnected
+                      ? SizedBox(
+                          height: 0,
+                        )
+                      : isIOS
+                          ? showCupertinoDialog(
+                              context: context,
+                              builder: (BuildContext context) =>
+                                  CupertinoAlertDialog(
+                                title: Icon(
+                                  Icons.error,
+                                  size: 40.0,
+                                  color: Colors.red,
+                                ),
+                                content: Text(
+                                  "Connessione Assente, alcuni servizi potrebbero essere bloccati!",
+                                  style: TextStyle(
+                                    fontSize: 27,
+                                  ),
+                                ),
+                                actions: [
+                                  CupertinoDialogAction(
+                                    child: Text(
+                                      "OK",
+                                      style: TextStyle(
+                                        fontSize: 28,
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.of(context, rootNavigator: true)
+                                          .pop('dialog');
+                                    },
+                                  )
+                                ],
+                              ),
+                            )
+                          : showDialog(
+                              barrierDismissible: false,
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                                title: Icon(
+                                  Icons.error,
+                                  size: 40.0,
+                                  color: Colors.red,
+                                ),
+                                content: Text(
+                                  "Connessione Assente, alcuni servizi potrebbero essere bloccati!",
+                                  style: TextStyle(
+                                    fontSize: 27,
+                                  ),
+                                ),
+                                actions: [
+                                  FlatButton(
+                                    child: Text(
+                                      "OK",
+                                      style: TextStyle(
+                                        fontSize: 28,
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.of(context, rootNavigator: true)
+                                          .pop('dialog');
+                                    },
+                                  )
+                                ],
+                              ),
+                            ),
+                  Image(
+                    image: AssetImage("assets/images/don_milani.png"),
+                    fit: BoxFit.cover,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 30, left: 15, right: 20),
+                    child: MainNews(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 30, left: 15, right: 20),
+                    child: MainBarbiana(),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 25,
+              ),
+            ],
+          ),
+        ));
   }
 }
