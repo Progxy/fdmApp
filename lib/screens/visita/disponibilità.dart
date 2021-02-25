78ab7cccaea3df645b4f3f3ea9009f5a885c13d8
TheProgxy <theprogxy@gmail.com>
2021-02-25 21:41:15 +0100

    fix in disponibilita.dart

--- lib/screens/visita/disponibilità.dart
+++ lib/screens/visita/disponibilità.dart
@@ -130,86 +130,84 @@ class _DisponibilitaState extends State<Disponibilita> {
             image: AssetImage("assets/images/don_milani.png"),
             fit: BoxFit.cover,
           ),
-          /*
-          SizedBox(
-            height: 25,
-          ),
-          FutureBuilder(
-            future: getData(database),
-            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-              return CarouselSlider(
-                items: data
-                    .map((i) => new Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 10,
-                              offset: Offset(0, 3),
-                            ),
-                          ],
-                          image: DecorationImage(
-                            image: AssetImage("assets/images/barbiana.jpg"),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        child: ListView(
-                          children: [
-                            Container(
-                              height: 450,
-                              width: 380,
-                              child: ListView(
-                                children: [
-                                  Row(
-                                    children: [
-                                      Container(
-                                        width: 275,
-                                        child: Align(
-                                          alignment: Alignment.topLeft,
-                                          child: Padding(
-                                            padding: const EdgeInsets.only(
-                                              left: 13.0,
-                                              top: 13.0,
-                                            ),
-                                            child: BorderedText(
-                                              strokeWidth: 4.0,
-                                              strokeColor: Colors.black,
-                                              child: Text(
-                                                "Il giorno " +
-                                                    i[0] +
-                                                    "\nci saranno i volontari : \n" +
-                                                    i[1],
-                                                style: TextStyle(
-                                                  fontSize: 28,
-                                                  fontWeight: FontWeight.w700,
-                                                  color: Colors.white,
-                                                ),
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        )))
-                    .toList(),
-                options: CarouselOptions(
-                    enlargeCenterPage: true,
-                    height: 450.0,
-                    aspectRatio: 4 / 3,
-                    enableInfiniteScroll: true,
-                    viewportFraction: 0.8),
-              );
-            },
-          ),
-        */
+          // SizedBox(
+          //   height: 25,
+          // ),
+          // FutureBuilder(
+          //   future: getData(database),
+          //   builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          //     return CarouselSlider(
+          //       items: data
+          //           .map((i) => new Container(
+          //               decoration: BoxDecoration(
+          //                 borderRadius: BorderRadius.circular(10),
+          //                 color: Colors.white,
+          //                 boxShadow: [
+          //                   BoxShadow(
+          //                     color: Colors.grey.withOpacity(0.5),
+          //                     spreadRadius: 5,
+          //                     blurRadius: 10,
+          //                     offset: Offset(0, 3),
+          //                   ),
+          //                 ],
+          //                 image: DecorationImage(
+          //                   image: AssetImage("assets/images/barbiana.jpg"),
+          //                   fit: BoxFit.cover,
+          //                 ),
+          //               ),
+          //               child: ListView(
+          //                 children: [
+          //                   Container(
+          //                     height: 450,
+          //                     width: 380,
+          //                     child: ListView(
+          //                       children: [
+          //                         Row(
+          //                           children: [
+          //                             Container(
+          //                               width: 275,
+          //                               child: Align(
+          //                                 alignment: Alignment.topLeft,
+          //                                 child: Padding(
+          //                                   padding: const EdgeInsets.only(
+          //                                     left: 13.0,
+          //                                     top: 13.0,
+          //                                   ),
+          //                                   child: BorderedText(
+          //                                     strokeWidth: 4.0,
+          //                                     strokeColor: Colors.black,
+          //                                     child: Text(
+          //                                       "Il giorno " +
+          //                                           i[0] +
+          //                                           "\nci saranno i volontari : \n" +
+          //                                           i[1],
+          //                                       style: TextStyle(
+          //                                         fontSize: 28,
+          //                                         fontWeight: FontWeight.w700,
+          //                                         color: Colors.white,
+          //                                       ),
+          //                                     ),
+          //                                   ),
+          //                                 ),
+          //                               ),
+          //                             ),
+          //                           ],
+          //                         ),
+          //                       ],
+          //                     ),
+          //                   ),
+          //                 ],
+          //               )))
+          //           .toList(),
+          //       options: CarouselOptions(
+          //           enlargeCenterPage: true,
+          //           height: 450.0,
+          //           aspectRatio: 4 / 3,
+          //           enableInfiniteScroll: true,
+          //           viewportFraction: 0.8),
+          //     );
+          //   },
+          // ),
         ],
       ),
     );
